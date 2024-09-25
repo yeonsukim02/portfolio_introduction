47ca71a3dac9d56fd819ab8e0813c1c51c67ff33
yeonsukim02 <gkfn3470@gmail.com>
2024-09-25 13:52:52 +0900

    Add files via upload

--- script/common.js
+++ script/common.js
@@ -1,4 +1,15 @@
-const portfolio_wrap = new Swiper('.portfolio_wrap',{
+const portfolio = new Swiper('.portfolio',{
     direction:'vertical',
     mousewheel:true,
+    on: {
+        slideChange: function () {
+            const currentSlide = this.activeIndex;
+            if (currentSlide === 0) {
+                document.querySelector('header').style.backgroundColor = 'transparent';
+            }
+            else if (currentSlide === 1) {
+                document.querySelector('header').style.backgroundColor = '#B22B15';
+            }
+        }
+    }
 })
